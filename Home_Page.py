9d764d92f3a0d467db9d4473132ba2e6bbc80957
Ak-33 <112743875+Ak-33@users.noreply.github.com>
2024-03-08 22:49:54 -0500

    Add files via upload

--- Home_Page.py
+++ Home_Page.py
@@ -162,7 +162,7 @@ jaknkifeall='pages/jaknkife/jaknkife-all.txt'
 
 
 st.set_page_config(
-    page_title="McCall Staff Rankings - V:PEW PEW 22s DAD CLAPS",
+    page_title="McCall Staff Rankings - V:1.5.0",
     page_icon="🔫",
     layout= "wide"
 )
